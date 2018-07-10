3c450e95e2fcf00dbf0efc5831f8c133da67b0ff
Thomas Chen <31905576+thomas-chen-12@users.noreply.github.com>
2018-07-09 20:33:56 -0700

    Added some dummy menu items

--- example.py
+++ example.py
@@ -1,8 +1,30 @@
 """
 Example program (exercise) for GUI in Python
 """
-import tkinter
+from tkinter import *
 
-top = tkinter.Tk()
+def foo():
+    print("Welcome to the best project in Capstone Expo")
 
-top.mainloop()
+# Set the window title and size
+root = Tk()
+root.title("Vector Signal Generator (Beta)")
+root.geometry("1280x720")
+root.resizable(True, True)
+
+# Add main menu and its sub-menus to the root
+main_menu = Menu(root)
+root.config(menu=main_menu)
+file_menu = Menu(main_menu, tearoff=False)
+instrument_menu = Menu(main_menu, tearoff=False)
+file_menu.add_command(label="New Project", command=foo)
+file_menu.add_command(label="Save", command=foo)
+file_menu.add_command(label="Save As...", command=foo)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=quit)
+instrument_menu.add_command(label="Add Instrument", command=foo)
+instrument_menu.add_command(label="Find Instrument", command=foo)
+main_menu.add_cascade(label="File", menu=file_menu)
+main_menu.add_cascade(label="Instrument", menu=instrument_menu)
+
+root.mainloop()
