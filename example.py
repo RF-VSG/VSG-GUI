d97ca45f334ac4f6d0f52b1b4e07d0834a309547
Thomas Chen <31905576+thomas-chen-12@users.noreply.github.com>
2018-07-18 00:05:05 -0700

    Added shapes and image in the window, just for practice

--- example.py
+++ example.py
@@ -3,6 +3,10 @@ Example program (exercise) for GUI in Python
 """
 from tkinter import *
 import tkinter.messagebox as messagebox
+import os.path
+
+canvas = None
+LIGHT_GREEN = "#CDEA90"         # TKinter does not support shapes with transparant colors. Use create_image() and attach a PNG file instead.
 
 def exit_warning():
     answer = messagebox.askquestion("Exit VSG", "Are you sure you want to exit the program?")
@@ -16,6 +20,9 @@ def foo():
 
 def show_message_box(title, message):
     messagebox.showinfo(title, message)
+
+def magic():
+    canvas.delete(green_box)
 
 # Set the window title and size
 root = Tk()
@@ -44,11 +51,25 @@ btn_insert = Button(toolbar, text="Insert Something", command=lambda:show_messag
 btn_insert.pack(side=LEFT, padx=5, pady=5)
 btn_print = Button(toolbar, text="Print", command=foo)
 btn_print.pack(side=LEFT, padx=5, pady=5)
+btn_magic = Button(toolbar, text="Magic", command=magic)
+btn_magic.pack(side=LEFT, padx=5, pady=5)
 toolbar.pack(side=TOP, fill=X)
 
 # Add status bar
 # 'bg' stands for background. Make it SUNKEN. Anchor the text to left (West)
 status_bar = Label(root, text="Bored", bd=True, relief=SUNKEN, anchor=W)
 status_bar.pack(side=BOTTOM, fill=X)
+
+# Add canvas for shapes and graphics
+canvas = Canvas(root, width=640, height=360)
+canvas.pack()
+black_line = canvas.create_line(0,0, 640, 360)
+red_line = canvas.create_line(0, 360, 640, 0, fill="red")
+green_box = canvas.create_rectangle(160, 90, 480, 270, fill=LIGHT_GREEN)
+
+# Add meme
+meme = PhotoImage(file=os.path.join(os.path.dirname(os.path.realpath(__file__)), "img","meme.png"))
+label_meme = Label(root, image=meme)
+label_meme.pack()
 
 root.mainloop()
